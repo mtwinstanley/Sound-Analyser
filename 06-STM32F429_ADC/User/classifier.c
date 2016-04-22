67a5fbc431d9958c9d31d512b8c467ce8b35347b
Matt Winstanley <mtwinstanley@hotmail.co.uk>
2016-04-22 16:55:51 +0100

    Updates to classifier for new hardware

--- 06-STM32F429_ADC/User/classifier.c
+++ 06-STM32F429_ADC/User/classifier.c
@@ -8,17 +8,17 @@ const char * classifier_decisionTree(uint32_t SMatrix[]){
 	char str[15];
 	const char * c;
 	LED_resetALL();
-	if (SMatrix[3] > 2 * SMatrix[5] && SMatrix[3] > SMatrix[4] && SMatrix[3] > SMatrix[2] && SMatrix[4] > SMatrix[5] && SMatrix[4] > SMatrix[1] && SMatrix[5] > SMatrix[1]){
+if (SMatrix[3] > SMatrix[5] && SMatrix[3] > SMatrix[4] && SMatrix[3] > SMatrix[2] && SMatrix[4] > SMatrix[5] && SMatrix[4] > SMatrix[1] && SMatrix[5] > SMatrix[1]){
 		LED_setLED(LED_GREEN_1);
 		sprintf(str, "BH\n\r");
 		c = "BH    ";
 	}
-	else if (SMatrix[5] > 2* SMatrix[3] && SMatrix[3] > 5 * SMatrix[2] && SMatrix[6] > SMatrix[2]){
+	else if (SMatrix[5] > SMatrix[3] && SMatrix[3] > 4 * SMatrix[2] && SMatrix[6] > SMatrix[2]){
 		LED_setLED(LED_GREEN_2);
 		sprintf(str, "FL\n\r");
 		c = "FL    ";
 	}
-	else if (SMatrix[5] >  4 * SMatrix[2] && SMatrix[2] > SMatrix[6] && SMatrix[9] > SMatrix[8] && SMatrix[3] > 4*SMatrix[2]){
+	else if (SMatrix[5] >  3 * SMatrix[2] && SMatrix[2] > SMatrix[6] && SMatrix[9] > SMatrix[8] && SMatrix[3] > 2*SMatrix[2]){
 		LED_setLED(LED_GREEN_3);
 		sprintf(str, "JP\n\r");
 		c = "JP    ";
